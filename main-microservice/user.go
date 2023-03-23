1b6b78625547d950f193b630ed24166472b4956a
guddytech <guddytechs@gmail.com>
2023-03-23 22:04:57 +0100

    added the Go codes for the remaining microservices

--- main-microservice/user.go
+++ main-microservice/user.go
@@ -1,19 +1,28 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"github.com/gin-gonic/gin"
+    "encoding/json"
+    "log"
+    "net/http"
 )
 
+type User struct {
+    ID    int    `json:"id"`
+    Name  string `json:"name"`
+    Email string `json:"email"`
+}
+
+var users = []User{
+    {ID: 1, Name: "John", Email: "john@example.com"},
+    {ID: 2, Name: "Jane", Email: "jane@example.com"},
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(users)
+}
+
 func main() {
-	r := gin.Default()
-
-	r.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello, world!",
-		})
-	})
-
-	log.Fatal(r.Run(":8080"))
+    http.HandleFunc("/users", getUsers)
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
